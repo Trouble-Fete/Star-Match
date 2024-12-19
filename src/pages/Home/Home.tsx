b24df4fa70b6322fb95bcdf768e04bd6d4fd59d6
Trouble-Fete <neyroud.thomas@gmail.com>
2024-12-20 00:30:26 +0100

    page home bien avancé

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,71 +2,102 @@ import "./Home.css";
 
 function Home() {
 	return (
-		<body>
+		<div id="home">
 			<header className="headerHome">
-				<h1>Star Match</h1>
-				<article>
-					<h2>Unlock the Galaxy of Love</h2>
-					<p>
-						Embark on a cosmic journey to find your soulmate in the vast expanse
-						of the Star Wars universe
-					</p>
-					<button type="button">Join now</button>
-				</article>
+				<section className="container1">
+					<h1>Star Match</h1>
+					<article>
+						<h2>Unlock the Galaxy of Love</h2>
+						<p>
+							Embark on a cosmic journey to find your soulmate in the vast
+							expanse of the Star Wars universe
+						</p>
+						<button type="button">Join now</button>
+					</article>
+				</section>
 			</header>
 
 			<main>
 				<h2>Explore Our Galactic Singles</h2>
 				<section className="topPage">
-					<article className="firstImage">
-						<img src="" alt="" />
-						<h3>Meet Your Cosmic</h3>
-						<h4>Uncover the secrets of the Force and find your perfect</h4>
-						<h4>Discover Your Destiny</h4>
-						<p>
-							Venture into the unknown and connect with like-minded individuals
-							who share your passion for the galaxy far, far away. Whether
-							you're a Jedi, a Sith, or simply a galactic wanderer, Galaxy Love
-							offers a safe and exciting platform to explore the possibilities
-							of love among the stars.
-						</p>
+					<article className="article1">
+						<img
+							className="firstImage"
+							src="/src/assets/vue_de_profil.jpg"
+							alt="homme"
+						/>
+						<section className="section1">
+							<h3>Meet Your Cosmic</h3>
+							<h4 className="grey">
+								Uncover the secrets of the Force and find your perfect
+							</h4>
+							<h4 className="pink">Discover Your Destiny</h4>
+							<p>
+								Venture into the unknown and connect with like-minded
+								individuals who share your passion for the galaxy far, far away.
+								Whether you're a Jedi, a Sith, or simply a galactic wanderer,
+								Galaxy Love offers a safe and exciting platform to explore the
+								possibilities of love among the stars.
+							</p>
+						</section>
 					</article>
-					<article className="secondImage">
-						<img src="" alt="" />
-						<h3>Ignite the Spark of </h3>
-						<h4>Navigate the Cosmic Landscape of Love</h4>
-						<h4>Embrace the Power of the Force</h4>
-						<p>
-							Forge Unbreakable Bonds: At Galaxy Love, we believe that true love
-							transcends the boundaries of the physical world. Our advanced
-							algorithms and intuitive matching system will help you discover
-							your soulmate, no matter how far they may be across the galaxy.
-						</p>
+					<article className="article2">
+						<img
+							className="secondImage"
+							src="/src/assets/femme_profile.png"
+							alt="femme"
+						/>
+						<section className="section2">
+							<h3>Ignite the Spark of </h3>
+							<h4 className="grey">Navigate the Cosmic Landscape of Love</h4>
+							<h4 className="pink">Embrace the Power of the Force</h4>
+							<p>
+								Forge Unbreakable Bonds: At Galaxy Love, we believe that true
+								love transcends the boundaries of the physical world. Our
+								advanced algorithms and intuitive matching system will help you
+								discover your soulmate, no matter how far they may be across the
+								galaxy.
+							</p>
+						</section>
 					</article>
 					<button type="button">Connect Now</button>
 				</section>
+
+				<h2>Discover Your Intestellar Destiny</h2>
 				<section className="midPage">
-					<h2>Discover Your Intestellar Destiny</h2>
-					<img src="" alt="" />
-					<h3>Ignite the Spark of</h3>
-					<p>Uncover the Mysteries of the Force and Let Love Bloom</p>
-					<div>
-						<img src="" alt="" />
-						<p>
-							Navigating the Cosmic Landscape of Love: Galaxy Love is your
-							gateway to discovering your soulmate in the expansive Star Wars
-							universe
-						</p>
-						<p>Forge Unbreakable Bonds Across the Galaxy</p>
-						<div>
-							<button type="button">Connect Now</button>
-							<button type="button">Join the Adventure</button>
+					<article className="container3">
+						<img className="imageMid" src="/src/assets/r2d2.jpg" alt="femme" />
+						<div className="box3">
+							<h3>Ignite the Spark of</h3>
+							<p>Uncover the Mysteries of the Force and Let Love Bloom</p>
+							<div className="petit">
+								<img
+									className="mariage"
+									src="/src/assets/mariageDessin.png"
+									alt="mariage"
+								/>
+								<p>
+									Navigating the Cosmic Landscape of Love: Galaxy Love is your
+									gateway to discovering your soulmate in the expansive Star
+									Wars universe
+								</p>
+							</div>
+							<p>Forge Unbreakable Bonds Across the Galaxy</p>
+
+							<div className="doubleBouton">
+								<button className="b1" type="button">
+									Connect Now
+								</button>
+								<button className="b2" type="button">
+									Join the Adventure
+								</button>
+							</div>
 						</div>
-					</div>
+					</article>
 				</section>
 				<section className="bottomPage">
 					<div className="firstBrick">
-						<img src="" alt="" />
+						<img src="/src/assets/embrace.png" alt="" />
 						<h5>Embrace the Cosmic</h5>
 						<p>
 							Unlock the Secrets of the Force and Find Your Galactic Soulmate.
@@ -74,7 +105,7 @@ function Home() {
 						</p>
 					</div>
 					<div className="secondBrick">
-						<img src="" alt="" />
+						<img src="/src/assets/ignite-removebg-preview.png" alt="" />
 						<h5>Ignite the Spark of</h5>
 						<p>
 							Venture Into the Unknown and Discover Your Cosmic Companion.
@@ -82,7 +113,7 @@ function Home() {
 						</p>
 					</div>
 					<div className="thirdBrick">
-						<img src="" alt="" />
+						<img src="/src/assets/planete.png" alt="logo" />
 						<h5>IForge Unbreakable</h5>
 						<p>
 							Harness the Power of the Force to Uncover Your Soulmate. Galaxy
@@ -90,8 +121,13 @@ function Home() {
 						</p>
 					</div>
 				</section>
+				<img
+					className="footer"
+					src="/src/assets/galaxie_etoile.jpg"
+					alt="galaxie"
+				/>
 			</main>
-		</body>
+		</div>
 	);
 }
 
