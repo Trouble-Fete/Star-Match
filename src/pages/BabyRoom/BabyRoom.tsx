f07993118aacdf7cbc9624c75b05c0ad16d8ef84
YamsTheBe <Mariamadiaw.a@hotmail.fr>
2024-12-20 00:31:00 +0100

    This is my second commit

--- src/pages/BabyRoom/BabyRoom.tsx
+++ src/pages/BabyRoom/BabyRoom.tsx
@@ -2,35 +2,42 @@ import { useState } from "react";
 import "./BabyRoom.css";
 
 const BabyRoom: React.FC = () => {
-	const [isMatched, setIsMatched] = useState(false);
+	const [MatchedIndex, setIsMatched] = useState(false);
 
 	const handleMatch = () => {
 		setIsMatched(true);
 	};
 	return (
 		<div>
-			<h1>Bienvenue dans la BabyRoom!</h1>
+			<h1>la BabyRoom, </h1>
+			<h2> Ici, la magie des futurs bébés prend vie!</h2>
+
 			<div className="BabyRoom-container">
 				<div className="Matched">
 					<img
-						src="https://3dgraphiste.fr/wp-content/uploads/2015/07/personnage-3d-robot-starwars.jpg"
+						src="https://resize-elle.ladmedia.fr/rcrop/638,,forcex/img/var/plain_site/storage/images/loisirs/cinema/dossiers/que-sont-ils-devenus-les-acteurs-de-star-wars/natalie-portman/58558807-1-fre-FR/Natalie-Portman.jpg"
 						alt="Matched 1"
 						className="Matched-card"
 					/>
 					<img
-						src="https://3dgraphiste.fr/wp-content/uploads/2015/07/personnage-3d-robot-starwars.jpg"
+						src="https://t3.ftcdn.net/jpg/02/98/94/38/360_F_298943877_A4W7tVyZPCu6gNGuGXJUerZbXsWmblLb.jpg"
 						alt="Matched 2"
 						className="Matched-card"
 					/>
 				</div>
+
 				<div className="button-container">
 					<button type="button" onClick={handleMatch} className="match-button">
-						Match!
+						NPortman!
+					</button>
+					<button type="button" onClick={handleMatch} className="match-button">
+						MAN!
 					</button>
 				</div>
+
 				<div className="baby-photo-container">
 					<img
-						src="https://t3.ftcdn.net/jpg/02/98/94/38/360_F_298943877_A4W7tVyZPCu6gNGuGXJUerZbXsWmblLb.jpg"
+						src="https://preview.redd.it/star-wars-except-everyone-is-a-baby-v0-oymmc4s45v3a1.png?width=1024&format=png&auto=webp&s=4e0fa062d1e318c8df043aa1e9ec37865a39822c"
 						alt="Matched baby "
 						className="baby-photo"
 					/>
