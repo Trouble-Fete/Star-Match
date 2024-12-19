93f6099f59d96199903f9e8959c3f689225e7d97
BenardEmilien <emilien.benard2@gmail.com>
2024-12-20 00:49:23 +0100

    dodo time

--- src/components/listCards/ListCards.tsx
+++ src/components/listCards/ListCards.tsx
@@ -15,22 +15,30 @@ export default function ListCards({
 }: { cardList: CardProps[]; isSelectable: boolean }) {
 	const [selectedCount, setSelectedCount] = useState(0);
 
-	const handleCardSelection = (isSelected: boolean) => {
-		setSelectedCount((card) => (isSelected ? card + 1 : card - 1));
+	const [selectedIds, setSelectedIds] = useState<number[]>([]);
+
+	const handleCardSelection = (isSelected: boolean, cardId: number) => {
+		setSelectedCount((count) => (isSelected ? count + 1 : count - 1));
+		setSelectedIds((prevIds) =>
+			isSelected ? [...prevIds, cardId] : prevIds.filter((id) => id !== cardId),
+		);
 	};
 
 	return (
 		<div>
 			<div>Cartes sélectionnées : {selectedCount}</div>
+			<div>ID sélectionnés : {selectedIds.join(", ")}</div>
 			<div id="listCards">
 				{cardList.map((element) => (
 					<Card
-						isSelectable={isSelectable && selectedCount < 2} // Permet la sélection uniquement si le décompte est en dessous du maximum.
+						isSelectable={isSelectable && selectedCount < 2}
 						key={element.id}
 						imageUrl={element.imageUrl}
 						name={element.name}
 						birthYear={element.birth_year}
-						onSelectionChange={handleCardSelection}
+						onSelectionChange={(isSelected) =>
+							handleCardSelection(isSelected, element.id)
+						}
 					/>
 				))}
 			</div>
