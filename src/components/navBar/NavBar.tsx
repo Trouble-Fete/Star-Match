115454b196a28b267ac0b73e7be5642126cba08a
BrunoBernard979 <bruno.bernard33@gmail.com>
2024-12-20 11:37:42 +0100

    Navbar+Footer

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -1,5 +1,16 @@
 import "./NavBar.css";
+// J'aime ce projet !!!
 
 export default function NavBar() {
-	return <div id="navBar">NavBar</div>;
+	return (
+<nav id="navBar">
+    <img src="public/death-star-svgrepo-com.svg" alt="Logo" className="nav-logo" /> 
+    <ul className="nav-list">
+      <li>Accueil</li>
+      <li>Matches</li>
+      <li>A propos </li>
+      <li>Contacts</li>
+    </ul>
+</nav>
+	);
 }
