d88fbe57df499bbc520d47377db5473f87e21e59
YamsTheBe <Mariamadiaw.a@hotmail.fr>
2024-12-20 12:31:46 +0100

    Update

--- src/pages/BabyRoom/BabyRoom.tsx
+++ src/pages/BabyRoom/BabyRoom.tsx
@@ -2,49 +2,62 @@ import { useState } from "react";
 import "./BabyRoom.css";
 
 const BabyRoom: React.FC = () => {
-	const [MatchedIndex, setIsMatched] = useState(false);
+	const [PhotoIndex, setIsPhoto] = useState(false);
 
-	const handleMatch = () => {
-		setIsMatched(true);
+	const handlePhoto = () => {
+		setIsPhoto(true);
 	};
+
+
+
+	
 	return (
 		<div>
-			<h1>la BabyRoom, </h1>
+			<h1>la BabyRoom </h1>
 			<h2> Ici, la magie des futurs bébés prend vie!</h2>
 
-			<div className="BabyRoom-container">
-				<div className="Matched">
+
+
+
+{/* 
+			<div className="container">
+				<div className="photo-container">
 					<img
 						src="https://resize-elle.ladmedia.fr/rcrop/638,,forcex/img/var/plain_site/storage/images/loisirs/cinema/dossiers/que-sont-ils-devenus-les-acteurs-de-star-wars/natalie-portman/58558807-1-fre-FR/Natalie-Portman.jpg"
-						alt="Matched 1"
-						className="Matched-card"
+						alt="photo1"
+						className="photo"
 					/>
+
+					<div className="line line-left" />
+					<div className="horizontal-line horizontal-left" />
+				</div>
+
+				<div className="photo-container">
 					<img
 						src="https://t3.ftcdn.net/jpg/02/98/94/38/360_F_298943877_A4W7tVyZPCu6gNGuGXJUerZbXsWmblLb.jpg"
-						alt="Matched 2"
-						className="Matched-card"
+						alt="photo2"
+						className="photo"
 					/>
-				</div>
-
-				<div className="button-container">
-					<button type="button" onClick={handleMatch} className="match-button">
-						NPortman!
-					</button>
-					<button type="button" onClick={handleMatch} className="match-button">
-						MAN!
-					</button>
-				</div>
-
-				<div className="baby-photo-container">
-					<img
-						src="https://preview.redd.it/star-wars-except-everyone-is-a-baby-v0-oymmc4s45v3a1.png?width=1024&format=png&auto=webp&s=4e0fa062d1e318c8df043aa1e9ec37865a39822c"
-						alt="Matched baby "
-						className="baby-photo"
-					/>
-				</div>
-			</div>
+					<div className="line line-right" />
+					<div className="horizontal-line horizontal-right" />
+				</div> */}
+			{/* 	<button type="button" onClick={handleMatch} className="match-button">
+					Match
+					</button> */}
+			{/* </div> */}
 		</div>
 	);
 };
 
+/* 
+<div className="photo-container">
+					<img
+						src=""
+						alt="photo""
+						className="photoBaby"
+					/>
+						</div>
+ */
 export default BabyRoom;
+
+
